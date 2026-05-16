c78bc92ae702f50519fe860daafee029d2872ab1
Pierre Oosthuizen <oosthuizen.pierre@gmail.com>
2026-05-16 17:43:35 +0200

    mdviewer: 0.1.1 (Developer ID notarized)

--- Casks/mdviewer.rb
+++ Casks/mdviewer.rb
@@ -1,6 +1,6 @@
 cask "mdviewer" do
-  version "0.1.0"
-  sha256 "cf0cb5246ca9c6ef8c78ac6d09558cd3f41cb7b906d19d20ae6c7aafe0d37646"
+  version "0.1.1"
+  sha256 "75889585fe4ce4c1c6afa8f5f9bf84f143c3d4823a8038cd20ead392196fc3c0"
 
   url "https://github.com/ploutonconsulting/md_viewer/releases/download/v#{version}/MDViewer-#{version}.zip"
   name "MD Viewer"
